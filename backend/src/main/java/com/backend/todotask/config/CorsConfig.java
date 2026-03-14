975e15d729ce08084b6197043b6a5ef2eafd643a
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2026-03-14 15:08:37 +0530

    cors configure

--- backend/src/main/java/com/backend/todotask/config/CorsConfig.java
+++ backend/src/main/java/com/backend/todotask/config/CorsConfig.java
@@ -7,15 +7,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigure() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
+
+                registry.addMapping("/api/**")   // secure only API routes
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
